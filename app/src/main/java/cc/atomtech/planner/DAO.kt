405bd75f8165ded51ff8644389bbea845d695e9d
C4lopsitta <simone@atomtech.cc>
2024-01-11 17:33:05 +0100

    Updated Compose Compiler version to "1.5.3" to support newer kotlin-android

--- app/src/main/java/cc/atomtech/planner/DAO.kt
+++ app/src/main/java/cc/atomtech/planner/DAO.kt
@@ -8,11 +8,6 @@ interface RemindersDAO {
 
 }
 
-class Converters {
-   @TypeConverter
-   fun reminderTimestampsTo
-}
-
 @Database(entities = [Reminder::class], version = 1)
 abstract class DAO: RoomDatabase() {
    abstract fun reminders(): RemindersDAO
--- app/src/main/java/cc/atomtech/planner/dataEntities/Reminder.kt
+++ app/src/main/java/cc/atomtech/planner/dataEntities/Reminder.kt
@@ -2,23 +2,19 @@ package cc.atomtech.planner.dataEntities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Fts4
 import androidx.room.PrimaryKey
 
+@Fts4
 @Entity(tableName = "reminders")
 data class Reminder (
-   @PrimaryKey(autoGenerate = true) val id: Long,
-   @ColumnInfo()                    var title: String,
-   @ColumnInfo()                    var isCompleted: Boolean = false,
-   @ColumnInfo()                    var timestamps: ReminderTimestamps,
-   @ColumnInfo()                    var notifies: Boolean = false
-) {
-
-}
-
-data class ReminderTimestamps (
-   val creation: Long,
-   val completion: Long?,
-   val notifies: Long?
+   @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid") val id: Long,
+   @ColumnInfo()                                                var title: String,
+   @ColumnInfo()                                                var isCompleted: Boolean = false,
+   @ColumnInfo()                                                var creationDate: Long,
+   @ColumnInfo()                                                var completionDate: Long?,
+   @ColumnInfo()                                                var notificationDate: Long?,
+   @ColumnInfo()                                                var notifies: Boolean = false
 ) {
 
 }
